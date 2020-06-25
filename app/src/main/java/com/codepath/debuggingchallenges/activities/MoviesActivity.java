c98eeabcdd99a2aa3be02a701a7c16ad7b110a61
acantusoto <acs.cantu.soto@gmail.com>
2020-06-25 15:57:38 -0500

    Solved all the bugs - with some help :)

--- app/src/main/java/com/codepath/debuggingchallenges/activities/MoviesActivity.java
+++ app/src/main/java/com/codepath/debuggingchallenges/activities/MoviesActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.codepath.asynchttpclient.AsyncHttpClient;
@@ -25,7 +26,7 @@ public class MoviesActivity extends AppCompatActivity {
 
     RecyclerView rvMovies;
     MoviesAdapter adapter;
-    ArrayList<Movie> movies;
+    ArrayList<Movie> movies = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,11 +34,13 @@ public class MoviesActivity extends AppCompatActivity {
         setContentView(R.layout.activity_movies);
         rvMovies = findViewById(R.id.rvMovies);
 
+        movies = new ArrayList<>();
         fetchMovies();
         // Create the adapter to convert the array to views
         adapter = new MoviesAdapter(movies);
         // Attach the adapter to a ListView
         rvMovies.setAdapter(adapter);
+        rvMovies.setLayoutManager(new LinearLayoutManager(this));
     }
 
 
@@ -49,7 +52,8 @@ public class MoviesActivity extends AppCompatActivity {
             public void onSuccess(int statusCode, Headers headers, JSON response) {
                 try {
                     JSONArray moviesJson = response.jsonObject.getJSONArray("results");
-                    movies = Movie.fromJSONArray(moviesJson);
+                    movies.addAll(Movie.fromJSONArray(moviesJson));
+                    adapter.notifyDataSetChanged();
                 } catch (JSONException e) {
                     e.printStackTrace();
                 }
